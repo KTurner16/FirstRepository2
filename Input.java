0cf28f0ac7843ac5ecdeea532317f8d5785f390f
Kyle Turner <kylet2014@outlook.com>
2016-09-19 14:57:22 +0100

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,46 @@
+
+package input;
+
+import java.util.Scanner;
+
+
+public class Input {
+
+    public static void main(String[] args) {
+        
+        
+         Scanner myScanner = new Scanner(System.in);
+        
+        System.out.println("What is your name?");
+        
+        String answer = myScanner.nextLine();
+        
+        System.out.println("Hi there, + answer");
+        
+        System.out.println("What is your age?");       
+        
+        int age = myScanner.nextInt();
+        System.out.println("you are:" +age);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
